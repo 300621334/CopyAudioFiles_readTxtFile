af4c4c2d7f7d35255680cb05dd75acf6306d9adb
shafiqRBC <sur79@live.com>
2017-02-28 13:59:24 -0500

    First commit. Utility to copy - no querying.

---
+++ CopyFiles/Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace CopyFiles
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFrom = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.btnTo = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.lblFound = new System.Windows.Forms.Label();
+            this.lblNotFound = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFrom
+            // 
+            this.btnFrom.Location = new System.Drawing.Point(537, 29);
+            this.btnFrom.Name = "btnFrom";
+            this.btnFrom.Size = new System.Drawing.Size(75, 37);
+            this.btnFrom.TabIndex = 0;
+            this.btnFrom.Text = "Paths";
+            this.btnFrom.UseVisualStyleBackColor = true;
+            this.btnFrom.Click += new System.EventHandler(this.btnFrom_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(30, 37);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(491, 22);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(30, 112);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(491, 22);
+            this.textBox2.TabIndex = 2;
+            // 
+            // btnTo
+            // 
+            this.btnTo.Location = new System.Drawing.Point(537, 107);
+            this.btnTo.Name = "btnTo";
+            this.btnTo.Size = new System.Drawing.Size(75, 34);
+            this.btnTo.TabIndex = 3;
+            this.btnTo.Text = "Copy To";
+            this.btnTo.UseVisualStyleBackColor = true;
+            this.btnTo.Click += new System.EventHandler(this.btnTo_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(30, 153);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(175, 41);
+            this.btnStart.TabIndex = 4;
+            this.btnStart.Text = "Start Copying";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // lblFound
+            // 
+            this.lblFound.AutoSize = true;
+            this.lblFound.Location = new System.Drawing.Point(239, 77);
+            this.lblFound.Name = "lblFound";
+            this.lblFound.Size = new System.Drawing.Size(46, 17);
+            this.lblFound.TabIndex = 5;
+            this.lblFound.Text = "label1";
+            // 
+            // lblNotFound
+            // 
+            this.lblNotFound.AutoSize = true;
+            this.lblNotFound.Location = new System.Drawing.Point(239, 153);
+            this.lblNotFound.Name = "lblNotFound";
+            this.lblNotFound.Size = new System.Drawing.Size(46, 17);
+            this.lblNotFound.TabIndex = 6;
+            this.lblNotFound.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(631, 206);
+            this.Controls.Add(this.lblNotFound);
+            this.Controls.Add(this.lblFound);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnTo);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btnFrom);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFrom;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btnTo;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label lblFound;
+        private System.Windows.Forms.Label lblNotFound;
+    }
+}
+
---
+++ CopyFiles/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;//needed for File....
+//Overview of DialogueBoxes: https://msdn.microsoft.com/en-us/library/aa969773(v=vs.110).aspx
+
+namespace CopyFiles
+{
+    public partial class Form1 : Form
+    {
+        #region Global Vars
+        string fileName = "";
+        string folder = "";
+        string newFile = "";
+        string[] allPathsArray = null;
+        int missingFiles = 0;
+        int counter = 0;
+        //BackgroundWorker bw;
+        #endregion
+
+        #region on Form Load
+        public Form1()
+        {
+            InitializeComponent();
+            lblFound.Text = "";
+            lblNotFound.Text = "";
+            ////======================================
+            //bw = new BackgroundWorker();
+            //bw.WorkerReportsProgress = true;
+            //bw.WorkerSupportsCancellation = true;
+            //bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            ////======================================
+        }
+        #endregion
+
+        #region click From-btn
+        private void btnFrom_Click(object sender, EventArgs e)
+        {
+            btnStart.Text = "Start Copying";
+            OpenFileDialog dlgFrom = new OpenFileDialog();
+            DialogResult x = dlgFrom.ShowDialog();
+            if(x == DialogResult.OK)
+            {
+             
+                fileName = dlgFrom.FileName;
+                textBox1.Text = fileName;
+                lblFound.Text = "";
+                lblNotFound.Text = "";
+                newFile = "";
+                allPathsArray = null;
+                missingFiles = 0;
+                counter = 0;
+            }
+            
+        }
+        #endregion
+
+        #region click To-btn
+        private void btnTo_Click(object sender, EventArgs e)
+        {
+            btnStart.Text = "Start Copying";
+            FolderBrowserDialog dlgTo = new FolderBrowserDialog();
+            DialogResult x = dlgTo.ShowDialog();
+            if (x == DialogResult.OK) //if (d.ShowDialog() == DialogResult.OK)
+            {
+                folder = dlgTo.SelectedPath;
+                textBox2.Text = folder;
+            }
+        }
+        #endregion
+
+        #region click StartCopying btn
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (fileName == "")
+            {
+                btnStart.Text = "Please select a file!";
+                return;
+            }
+            if(folder=="")
+            {
+                btnStart.Text = "Please select a folder!";
+                return;
+            }
+            btnFrom.Enabled  = false;
+            btnTo.Enabled    = false;
+            btnStart.Enabled = false;
+            startCopying();
+
+           //if(bw.IsBusy != true)
+           //{
+           //    bw.RunWorkerAsync();
+           //}
+        }
+        #endregion
+
+        #region event-Method to startCopying
+        //private void bw_DoWork(Object sender, DoWorkEventArgs e)
+        //{
+        //    allPathsArray = File.ReadAllLines(fileName, Encoding.UTF8);
+        //    lblFound.Text = "Files Found: " + allPathsArray.Length;
+
+        //    foreach (string path in allPathsArray)
+        //    {
+        //        newFile = folder + "\\" + path.Substring(path.LastIndexOf('\\') + 1);
+        //        try
+        //        {
+        //            File.Copy(path, newFile, true);
+        //        }
+        //        catch (FileNotFoundException ex)
+        //        {
+        //            missingFiles++;
+        //            continue;
+        //        }
+        //    }
+        //    lblNotFound.Text = " Files NOT found: " + missingFiles;
+        //    btnFrom.Enabled = true;
+        //    btnTo.Enabled = true;
+        //    btnStart.Enabled = true;
+        //}
+        #endregion
+
+        #region Method StartCopying
+        private void startCopying()
+        {
+            allPathsArray = File.ReadAllLines(fileName, Encoding.UTF8);
+            
+
+            foreach (string path in allPathsArray)
+            {
+                if(!string.IsNullOrWhiteSpace(path))
+                {
+                newFile = folder + "\\" + path.Substring(path.LastIndexOf('\\') + 1);
+                try
+                {
+                    File.Copy(path, newFile, true);
+                    counter++;
+                }
+                catch (FileNotFoundException ex)
+                {
+                    missingFiles++;
+                    continue;
+                }
+                }
+            }
+            lblFound.Text = "Files Found: " + counter;
+            counter = 0;
+            lblNotFound.Text = " Files NOT found: " + missingFiles;
+            btnFrom.Enabled = true;
+            btnTo.Enabled = true;
+            btnStart.Enabled = true;
+        }
+        #endregion
+
+    }
+}
